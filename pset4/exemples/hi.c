eca9ecfa3f2ffd2e7a53735715bd7ca31ca9efa2
levy_m <88509014+LevyMatias@users.noreply.github.com>
2023-06-21 18:45:07 +0000

    Wed, Jun 21, 2023, 3:45 PM -03:00

--- pset4/exemples/hi.c
+++ pset4/exemples/hi.c
@@ -11,4 +11,8 @@ int main(void)
     printf("%c\n", s[0]);
     printf("%c\n", s[1]);
     printf("%c\n", s[2]);
+    
+    printf("%c\n", *s);
+    printf("%c\n", *(s+1));
+    printf("%c\n", *(s+2));
 }
