70947b733e78729a157fc9220d0caa3dace451c9
levy_m <88509014+LevyMatias@users.noreply.github.com>
2023-06-21 18:35:35 +0000

    Wed, Jun 21, 2023, 3:35 PM -03:00

--- pset4/exemples/hi.c
+++ pset4/exemples/hi.c
@@ -1,8 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void)
+/*int main(void)
 {
     string s = "HI!";
     printf("%s\n", s);
+}*/
+
+int main(void)
+{
+    char *s = "HI!";
+    printf("%c\n", s[0]);
+    printf("%c\n", s[1]);
+    printf("%c\n", s[2]);
 }
