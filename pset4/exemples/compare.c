b79f135defba4ac2a6bbf172ad8e115bf0012e9e
levy_m <88509014+LevyMatias@users.noreply.github.com>
2023-06-21 19:14:54 +0000

    Wed, Jun 21, 2023, 4:14 PM -03:00

--- pset4/exemples/compare.c
+++ pset4/exemples/compare.c
@@ -1,11 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
-
+#include <string.h>
 int main(void)
 {
-    char *s = get_int("s: ");
-    char *t = get_int("t: ");
-    if (s == t)
+    char *s = get_string("s: ");
+    char *t = get_string("t: ");
+    if (strcmp(s, t) == 0)
     {
          printf("Same\n");
     }
