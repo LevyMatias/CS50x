a5d4fe3b1c192f7c098c1aec8642a7e7c27713c7
levy_m <88509014+LevyMatias@users.noreply.github.com>
2023-06-22 18:46:31 +0000

    Thu, Jun 22, 2023, 3:46 PM -03:00

---
+++ pset4/exemples/copy.c
@@ -0,0 +1,12 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+
+int main(void)
+{
+    char *s = get_string("s: ");
+    char *t = s;
+    t[0] = toupper(t[0]);
+    printf("s: %s\n", s);
+    printf("t: %s\n", t);
+} 
