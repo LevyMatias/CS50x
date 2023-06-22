72b0c14d5796b4784f75f9b8a9f90404aaa01312
levy_m <88509014+LevyMatias@users.noreply.github.com>
2023-06-22 18:58:42 +0000

    Thu, Jun 22, 2023, 3:58 PM -03:00

--- pset4/exemples/copy.c
+++ pset4/exemples/copy.c
@@ -1,12 +1,21 @@
 #include <cs50.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-    char *s = get_string("s: ");
-    char *t = s;
+    // Get a string
+    string s = get_string("s: ");
+
+    // Copy string's address
+    string t = s;
+
+    // Capitalize first letter in string
     t[0] = toupper(t[0]);
+
+    // Print string twice
     printf("s: %s\n", s);
     printf("t: %s\n", t);
 }
+
