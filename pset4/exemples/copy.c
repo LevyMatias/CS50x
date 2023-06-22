7e81a4529e6db5a6bf9f082d4314808e8a980b91
levy_m <88509014+LevyMatias@users.noreply.github.com>
2023-06-22 19:32:47 +0000

    Thu, Jun 22, 2023, 4:32 PM -03:00

--- pset4/exemples/copy.c
+++ pset4/exemples/copy.c
@@ -1,21 +1,60 @@
 #include <cs50.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main(void)
 {
     // Get a string
-    string s = get_string("s: ");
+    char *s = get_string("s: ");
+    if (s == NULL)
+    {
+        return 1;
+    }
 
-    // Copy string's address
-    string t = s;
+    // Allocate memory for another string
+    char *t = malloc(strlen(s) + 1);
+    if (t == NULL)
+    {
+        return 1;
+    }
 
-    // Capitalize first letter in string
-    t[0] = toupper(t[0]);
+    // Copy string into memory
+    strcpy(t, s);
 
-    // Print string twice
+    // Capitalize copy
+    if (strlen(t) > 0)
+    {
+        t[0] = toupper(t[0]);
+    }
+
+    // Print strings
     printf("s: %s\n", s);
     printf("t: %s\n", t);
+
+    // Free memory
+    free(t);
+    return 0;
 }
 
+/*int main(void)
+{
+     char *s = get_string("s: ");
+     char *t = malloc(strlen(s) + 1);
+     if (t == NULL)
+     {
+          return 1;
+     }
+     for (int i = 0, n = strlen(s); i < n + 1; i++)
+     {
+          t[i] = s[i];
+     }
+     if (strlen(t) > 0)
+     {
+          t[0] = toupper(t[0]);
+     }
+     printf("s: %s\n", s);
+     printf("t: %s\n", t);
+     free(t);
+}*/
