1dcf46b2803a7da0be1b42371b47fbf83d623db0
levy_m <88509014+LevyMatias@users.noreply.github.com>
2023-07-05 22:01:29 +0000

    Wed, Jul 5, 2023, 7:01 PM -03:00

---
+++ pset4/exemples/scanf.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char s[4];
+    printf("s: ");
+    scanf("%s", s);
+    printf("s: %s\n", s);
+}
+
+/*#include <stdio.h>
+
+int main(void)
+{
+    char *s;
+    printf("s: ");
+    scanf("%s", s);
+    printf("s: %s\n", s);
+}
+
+*/
