9fa7f1e3a13d39834fe081afd3c1234fea006758
levy_m <88509014+LevyMatias@users.noreply.github.com>
2023-07-12 22:54:07 +0000

    Wed, Jul 12, 2023, 7:54 PM -03:00

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -7,14 +7,14 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            RGBTRIPPLE
+            RGBTRIPLE pixel = image[i][j];
             // Calcula o valor médio dos componentes de cor RGB
-            int avg = (image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3;
+            int avg = (pixel.rgbtRed + pixel.rgbtGreen + pixel.rgbtBlue) / 3;
 
             // Define o valor médio como o novo valor para cada componente de cor RGB
-            image[i][j].rgbtRed = avg;
-            image[i][j].rgbtGreen = avg;
-            image[i][j].rgbtBlue = avg;
+            pixel.rgbtRed = avg;
+            pixel.rgbtGreen = avg;
+            pixel.rgbtBlue = avg;
         }
     }
     return;
