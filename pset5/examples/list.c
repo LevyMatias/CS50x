72d7dc0fdc62e0ac8268491cd525476117e5d737
levy_m <88509014+LevyMatias@users.noreply.github.com>
2023-08-01 21:55:44 +0000

    Tue, Aug 1, 2023, 6:55 PM -03:00

--- pset5/examples/list.c
+++ pset5/examples/list.c
@@ -1,6 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
+{
+    // List of size 3
+    int *list = malloc(3 * sizeof(int));
+    if (list == NULL)
+    {
+        return 1;
+    }
+
+    list[0] = 1;
+    list[1] = 2;
+    list[2] = 3;
+
+    int *tmp = malloc(4 * sizeof(int));
+    if (tmp == NULL)
+    {
+        free(list);
+        return 1;
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        tmp[i] = list[i];
+    }
+
+    tmp[3] = 4;
+
+    free(list);
+
+    list = tmp;
+
+    // Print list
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%i\n", list[i]);
+    }
+
+    // Free list
+    free(list);
+    return 0;
+
+/*int main(void)
 {
     int list[3];
     list[0] = 1;
@@ -11,4 +53,4 @@ int main(void)
     {
         printf("%i\n", list[i]);
     }
-}
+}*/
