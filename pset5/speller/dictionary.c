674f0b04c3f711e6e63ca2cc0ba2f72c1cb0bdb1
levy_m <88509014+LevyMatias@users.noreply.github.com>
2023-08-10 18:09:06 +0000

    Thu, Aug 10, 2023, 3:09 PM -03:00

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -35,8 +35,45 @@ unsigned int hash(const char *word)
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    // TODO
-    return false;
+    // Open dictionary file
+    FILE *file = fopen(dictionary, "r");
+    if (file == NULL)
+    {
+        return false;
+    }
+
+    // Initialize hash table
+    for (int i = 0; i < HASH_TABLE_SIZE; i++)
+    {
+        hash_table[i] = NULL;
+    }
+
+    // Read words from dictionary and insert into hash table
+    char word[MAX_WORD_LENGTH + 1];
+    while (fscanf(file, "%s", word) != EOF)
+    {
+        // Create a new node for the word
+        node *new_node = malloc(sizeof(node));
+        if (new_node == NULL)
+        {
+            fclose(file);
+            return false;
+        }
+
+        // Copy the word into the new node
+        strcpy(new_node->word, word);
+
+        // Hash the word to get an index
+        int index = hash(word);
+
+        // Insert the new node at the beginning of the linked list at the index
+        new_node->next = hash_table[index];
+        hash_table[index] = new_node;
+    }
+
+    fclose(file);
+
+    return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
