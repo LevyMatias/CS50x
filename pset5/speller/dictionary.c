99aaff78ac180d2473703c9a44ea7542d3f6634c
levy_m <88509014+LevyMatias@users.noreply.github.com>
2023-08-11 16:24:35 +0000

    Fri, Aug 11, 2023, 1:24 PM -03:00

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -23,7 +23,8 @@ node *table[N];
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
-    // TODO
+    int index = hash(word);
+    node *cursor = table[index];
     return false;
 }
 
