6509a7f9e7e74ebb08c8e1f1c16eab4f243022bb
levy_m <88509014+LevyMatias@users.noreply.github.com>
2023-08-11 16:27:30 +0000

    Fri, Aug 11, 2023, 1:27 PM -03:00

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -25,6 +25,12 @@ bool check(const char *word)
 {
     int index = hash(word);
     node *cursor = table[index];
+    while(cursor != NULL) {
+        if(strcasecmp(cursor->word, word) == 0) {
+            return true;
+        }
+        cursor = cursor->next;
+    }
     return false;
 }
 
