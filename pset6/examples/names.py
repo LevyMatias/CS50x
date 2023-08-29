1aba2b9dda1641e950930754bc6c8e3906d85e28
levy_m <88509014+LevyMatias@users.noreply.github.com>
2023-08-29 21:18:42 +0000

    Tue, Aug 29, 2023, 6:18 PM -03:00

---
+++ pset6/examples/names.py
@@ -0,0 +1,13 @@
+import sys
+
+# A list of names
+names = ["Bill", "Charlie", "Fred", "George", "Ginny", "Percy", "Ron"]
+
+name = input("Name: ")
+
+if name in names:
+    print("Found")
+    sys.exit(0)
+
+print("Not found")
+sys.exit(1)
