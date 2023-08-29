f7ab9ac1a2fbbd4eca798b4affe96c74a9c34362
levy_m <88509014+LevyMatias@users.noreply.github.com>
2023-08-29 21:32:34 +0000

    Tue, Aug 29, 2023, 6:32 PM -03:00

--- pset6/examples/phonebook.py
+++ pset6/examples/phonebook.py
@@ -1,11 +1,12 @@
-from cs50 import get_string
+import csv
 
-people = {
-    "Carter": "+1-617-495-1000",
-    "David": "+1-949-468-2750"
-}
+# Get name and number
+name = input("Name: ")
+number = input("Number: ")
 
-# Search for name
-name = get_string("Name: ")
-if name in people:
-    print(f"Number: {people[name]}")
+# Open CSV file
+with open("phonebook.csv", "a") as file:
+
+    # Print to file
+    writer = csv.DictWriter(file, fieldnames=["name", "number"])
+    writer.writerow({"name": name, "number": number})
