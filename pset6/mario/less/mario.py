dd70628965a7e249942899cda2ef23d7aa813dc7
levy_m <88509014+LevyMatias@users.noreply.github.com>
2023-08-30 15:26:13 +0000

    Wed, Aug 30, 2023, 12:26 PM -03:00

--- pset6/mario/less/mario.py
+++ pset6/mario/less/mario.py
@@ -1,8 +1,8 @@
 from cs50 import get_int
 
 n = get_int("Height: ")
-while n < 8 && !0
-for i in range(8):
-    for j in range(8):
-        print("#", end="")
-    print()
+while n < 8 and not n == 0:
+    for i in range(8):
+        for j in range(8):
+            print("#", end="")
+        print()
