cdd6b5b3ef2780f47d8e463aa4e7300097c30d94
levy_m <88509014+LevyMatias@users.noreply.github.com>
2023-08-30 16:02:49 +0000

    Wed, Aug 30, 2023, 1:02 PM -03:00

--- pset6/mario/less/mario.py
+++ pset6/mario/less/mario.py
@@ -6,10 +6,12 @@ def print_pyramid(height):
         hashes = "#" * (i + 1)
         print(spaces + hashes)
 
-height = get_int("Height: ")
-if height > 8 or height == 0:
-    while True:
-        height = get_int("Height: ")  # Solicita a altura da pirâmide ao usuário
-        break
+def main():
+    height = get_int("Height: ")
+    if height > 8 or height == 0:
+        while True:
+            height = get_int("Height: ")
+            break
+    print_pyramid(height)
 
-print_pyramid(height)
+main()
