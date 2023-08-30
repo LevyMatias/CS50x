11673c05606efae0f61c53e6e2fcfb1b0fb64ba2
levy_m <88509014+LevyMatias@users.noreply.github.com>
2023-08-30 15:32:36 +0000

    Wed, Aug 30, 2023, 12:32 PM -03:00

--- pset6/mario/less/mario.py
+++ pset6/mario/less/mario.py
@@ -1,11 +1,18 @@
 from cs50 import get_int
 
 n = get_int("Height: ")
-if n > 8 and n == 0:
+
+if n <= 8 and n != 0:
+    for i in range(n):
+        for j in range(8):
+            print("#", end="")
+        print()
+else:
     while True:
         n = get_int("Height: ")
-    elif:
-        for i in range(8):
-            for j in range(8):
-                print("#", end="")
-            print()
+        if n <= 8 and n != 0:
+            for i in range(n):
+                for j in range(8):
+                    print("#", end="")
+                print()
+            break  # Saia do loop após a entrada correta
