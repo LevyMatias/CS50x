e3b776070d4300f54fe9161c7c77bf3c35ad6e22
levy_m <88509014+LevyMatias@users.noreply.github.com>
2023-08-30 19:06:49 +0000

    Wed, Aug 30, 2023, 4:06 PM -03:00

--- pset6/mario/less/mario.py
+++ pset6/mario/less/mario.py
@@ -1,6 +1,5 @@
 from cs50 import get_int
 
-
 def print_pyramid(height):
     for i in range(height):
         spaces = " " * (height - i - 1)
@@ -8,14 +7,11 @@ def print_pyramid(height):
         print(spaces + hashes, "\n")
 
 # main function
-
-
 def main():
     while True:
         height = get_int("Height: ")
         if height <= 8 and not height <= 0:
             break  # Sai do loop se a entrada for válida
     print_pyramid(height)
-
 
 main()
