8b088b33deddac98554c7ac63ec2f7fb715c6250
levy_m <88509014+LevyMatias@users.noreply.github.com>
2023-09-01 21:46:01 +0000

    Fri, Sep 1, 2023, 9:46 PM +00:00

--- pset6/mario/more/mario.py
+++ pset6/mario/more/mario.py
@@ -1,1 +1,10 @@
 # Exercício 2: Mario(versão desafiadora)
+# main function
+def main():
+    while True:
+        height = get_int("Height: ")
+        if height <= 8 and not height <= 0:
+            break  # Sai do loop se a entrada for válida
+    print_pyramid(height)
+
+main()
